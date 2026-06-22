e78e50ce10991a4290e98226fba6296880923876
diddydooright <285105561+diddydooright@users.noreply.github.com>
2026-06-22 01:48:09 -0500

    nothing

--- src/main/java/sigma/ligma/blacklistfricker/BlacklistFricker.java
+++ src/main/java/sigma/ligma/blacklistfricker/BlacklistFricker.java
@@ -1,7 +1,6 @@
 package sigma.ligma.blacklistfricker;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- src/main/java/sigma/ligma/blacklistfricker/mixin/RemoveFilterMixin.java
+++ src/main/java/sigma/ligma/blacklistfricker/mixin/RemoveFilterMixin.java
@@ -1,10 +1,8 @@
 package sigma.ligma.blacklistfricker.mixin;
 
 import com.mojang.patchy.MojangBlockListSupplier;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-
 import java.util.function.Predicate;
 
 @Mixin(MojangBlockListSupplier.class)
